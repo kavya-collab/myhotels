a19ad6500b6433831398bc5e97cb3ee410e99868
Kavyach59 <Kavya.chapparapu@outlook.com>
2024-06-03 10:19:38 +0530

    Added favourite button in listingdetails page

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -96,7 +96,7 @@ const navigation = {
             ))}
           </div>
           <p className="mt-10 text-center text-xs leading-5 text-gray-500">
-            &copy; 2020 Your Company, Inc. All rights reserved.
+            &copy; 2K Your Company, Inc. All rights reserved.
           </p>
         </div>
       </footer>
--- components/ListingDetailsCard.tsx
+++ components/ListingDetailsCard.tsx
@@ -4,39 +4,45 @@ import { Separator } from "./ui/separator";
 import React from "react"
 import { ListingCardProps } from "./ListingCard";
 import ListingDetailsCardImages from "./ListingDetailsCardImages";
-import Link from "next/link";
+
+import ListingFavoriteButton from "./ListingFavoriteButton";
 
 
 
 const ListingDetailsCard: React.FC<ListingCardProps> = ({ listing }) => {
     return (
         
-        <Card>
-            <ListingDetailsCardImages listing={listing} />
-            <Separator className='mb-4' />
-            <div className='flex flex-col gap-2'>
-                <h1 className='mb-2 text-2xl font-bold'>{listing.name}</h1>
-                <div className='flex items-center gap-2'>
-                    <DollarSign className='h-4 w-4 text-primary' />
-                    <span className='text-muted-foreground'>
-                        <span className='font-bold text-foreground'>{listing.price}</span> /
-                        night
-                    </span>
-                </div>
-                <div className='flex items-center gap-2'>
-                    <Pin className='h-4 w-4 text-primary' />
-                    <span className='text-muted-foreground'>{listing.location.name}</span>
-                </div>
-                <div className='flex items-center gap-2'>
-                    <Users className='h-4 w-4 text-primary' />
-                    <span className='text-muted-foreground'>
-                        {listing.maxGuests} Guests
-                    </span>
-                </div>
-            </div>
-            <Separator className='my-4' />
-            <div className='whitespace-pre-line'>{listing.description}</div>
-        </Card>
+        <Card className='mx-auto p-4'>
+      <ListingDetailsCardImages listing={listing} />
+      <Separator className='mb-4' />
+      <div className='flex justify-between'>
+        <div className='flex flex-col gap-2'>
+          <h1 className='mb-2 text-2xl font-bold'>{listing.name}</h1>
+          <div className='flex items-center gap-2'>
+            <DollarSign className='h-4 w-4 text-primary' />
+            <span className='text-muted-foreground'>
+              <span className='font-bold text-foreground'>{listing.price}</span>{' '}
+              / night
+            </span>
+          </div>
+          <div className='flex items-center gap-2'>
+            <Pin className='h-4 w-4 text-primary' />
+            <span className='text-muted-foreground'>
+              {listing.location.name}
+            </span>
+          </div>
+          <div className='flex items-center gap-2'>
+            <Users className='h-4 w-4 text-primary' />
+            <span className='text-muted-foreground'>
+              {listing.maxGuests} Guests
+            </span>
+          </div>
+        </div>
+        <ListingFavoriteButton listing={listing} />
+      </div>
+      <Separator className='my-4' />
+      <div className='whitespace-pre-line'>{listing.description}</div>
+    </Card>
         
     )
 }
